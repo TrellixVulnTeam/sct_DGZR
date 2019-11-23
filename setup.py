24b6f3963f66b205968889f141251cd546fd603b
Shankar <123456789987654321sr@gmail.com>
2019-11-23 22:46:58 +0530

    changed url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='sct',
       version='0.1',
       description='sct package for crypto tools',
-      url='https://github.com/ShankarCodes',
+      url='https://github.com/ShankarCodes/sct',
       author='shankar',
       author_email='mailstoshankar@gmail.com',
       license='MIT',
